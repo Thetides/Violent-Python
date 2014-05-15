44c283b237e161fabc321ed44243d33535505132
Thetides <luke.milby@gmail.com>
2014-05-15 16:57:27 -0500

    update to portscanner

--- chapter1/portscanner.py
+++ chapter1/portscanner.py
@@ -1,4 +1,7 @@
 import socket
+import os
+import sys
+
 def retBanner(ip,port):
 	try:
 		socket.setdefaulttimeout(2)
@@ -9,6 +12,13 @@ def retBanner(ip,port):
 	except:
 		return
 def checkVulns(banner):
+	f = open("vuln_banners.txt",'r')
+	for line in f.readlines():
+		if line.strip('\n') in banner:
+			print "[+] Server is vulnerable: "+banner.strip('\n')
+
+
+
 	if 'FreeFloat FTP Server (Version 1.00)' in banner:
 		print '[+] FreeFloat FTP Server is Vulnerable.'
 	elif '3Com 3CDaemon FTP Server Version 2.0' in banner:
@@ -21,13 +31,24 @@ def checkVulns(banner):
 		print '[+] FTP Server is not Vulnerable'
 	return
 def main():
-	portList = [21,22,25,80,110,443]
-	for x in range(1,255):
-		ip = '192.168.1.'+str(x)
-		for port in portList:
-			banner = retBanner(ip,port)
-			if banner:
-				print '[+] '+ ip + ': '+ banner
-				checkVulns(banner) 
+	if len(sys.argv) == 2:
+		filename = sys.argv[1]
+		if not os.path.isfile(filename):
+			print '[-] '+filename+' does not exist.'
+			exit(0)
+			if not os.access(filename, os.R_OK):
+				print '[-] '+ filename + ' access denied.'
+				exit(0)
+		else:
+			print '[-] Usage '+ str(sys.argv[0]) + ' <vuln filename>'
+			exit(0)
+			portList = [21,22,25,80,110,443]
+			for x in range(1,255):
+				ip = '192.168.1.'+str(x)
+				for port in portList:
+					banner = retBanner(ip,port)
+					if banner:
+						print '[+] '+ ip + ': '+ banner
+						checkVulns(banner) 
 if __name__ == '__main__':
 	main()
